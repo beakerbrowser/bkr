e034bb921f4959781cb4ee9f6db6260f2311f638
Paul Frazee <pfrazee@gmail.com>
2016-11-28 10:09:51 -0600

    factor out usage method

--- bin/bkr.js
+++ bin/bkr.js
@@ -17,6 +17,7 @@ import hostCmd from '../lib/commands/host'
 import unhostCmd from '../lib/commands/unhost'
 
 import * as errorHandler from '../lib/error-handler'
+import usage from '../lib/usage'
 
 // main
 // =
@@ -38,34 +39,11 @@ var commands = [
 ].map(errorHandler.wrapCommand)
 
 // match & run the command
-var match = subcommand({ commands, none })
+var defaults = [{ name: 'dir', default: process.cwd() }]
+var match = subcommand({ commands, none, defaults })
 match(process.argv.slice(2))
 
 function none (args) {
-  if (args._[0]) { 
-    console.log(chalk.red(`\nInvalid command: ${args._[0]}`))
-  } else {
-    console.log('')
-  }
-  console.log(`Usage: ${chalk.bold(`bkr`)} <command> ${chalk.gray(`[opts...]`)}
-
-  ${chalk.bold(`init`)} ${chalk.gray(`[directory]`)}
-  ${chalk.bold(`co`)} <dat-link> ${chalk.gray(`[directory]`)}
-  ${chalk.bold(`fork`)} <dat-link|directory> ${chalk.gray(`[directory]`)}
-
-  ${chalk.bold(`dev`)} ${chalk.gray(`[directory]`)}
-
-  ${chalk.bold(`status`)} ${chalk.gray(`[directory]`)}
-  ${chalk.bold(`pull`)} ${chalk.gray(`[--live] [directory]`)}
-  ${chalk.bold(`publish`)} ${chalk.gray(`[major|minor|patch|{version}] [directory]`)}
-
-  ${chalk.bold(`ls`)} ${chalk.gray(`[--mine]`)}
-  ${chalk.bold(`add`)} <dat-link|dat-ref>
-  ${chalk.bold(`rm`)} <dat-link|dat-ref>
-  ${chalk.bold(`host`)} <dat-link|dat-ref>
-  ${chalk.bold(`unhost`)} <dat-link|dat-ref>
-
-  ${chalk.blue(`Learn more at https://github.com/beakerbrowser/bkr`)}
-`)
-  process.exit(1)
+  var err = (args._[0]) ? `Invalid command: ${args._[0]}` : false
+  usage(err)
 }
