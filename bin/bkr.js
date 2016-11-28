8964a20686e9f14247be4211631351fe709332e8
Paul Frazee <pfrazee@gmail.com>
2016-11-27 21:30:38 -0600

    add command skeletons and usage help

--- bin/bkr.js
+++ bin/bkr.js
@@ -1,8 +1,61 @@
 #!/usr/bin/env node
 
-import minimist from 'minimist'
+import subcommand from 'subcommand'
 import chalk from 'chalk'
 
-const argv = minimist(process.argv.slice(2))
+import initCmd from '../lib/commands/init'
+import coCmd from '../lib/commands/co'
+import forkCmd from '../lib/commands/fork'
+import statusCmd from '../lib/commands/status'
+import pullCmd from '../lib/commands/pull'
+import publishCmd from '../lib/commands/publish'
+import devCmd from '../lib/commands/dev'
+import lsCmd from '../lib/commands/ls'
+import addCmd from '../lib/commands/add'
+import rmCmd from '../lib/commands/rm'
+import hostCmd from '../lib/commands/host'
+import unhostCmd from '../lib/commands/unhost'
 
-console.log(chalk.bold.red('bkr is merely a twinkle in prf\'s eye'))
+var match = subcommand({
+  commands: [
+    initCmd,
+    coCmd,
+    forkCmd,
+    statusCmd,
+    pullCmd,
+    publishCmd,
+    devCmd,
+    lsCmd,
+    addCmd,
+    rmCmd,
+    hostCmd,
+    unhostCmd
+  ],
+  none
+})
+match(process.argv.slice(2))
+
+
+function none () {
+  console.log(
+`Usage: ${chalk.bold(`bkr`)} <command> ${chalk.gray(`[opts...]`)}
+
+  ${chalk.bold(`init`)} ${chalk.gray(`[directory]`)}
+  ${chalk.bold(`co`)} <dat-link> ${chalk.gray(`[directory]`)}
+  ${chalk.bold(`fork`)} <dat-link|directory> ${chalk.gray(`[directory]`)}
+
+  ${chalk.bold(`dev`)} ${chalk.gray(`[directory]`)}
+
+  ${chalk.bold(`status`)} ${chalk.gray(`[directory]`)}
+  ${chalk.bold(`pull`)} ${chalk.gray(`[--live] [directory]`)}
+  ${chalk.bold(`publish`)} ${chalk.gray(`[major|minor|patch|{version}] [directory]`)}
+
+  ${chalk.bold(`ls`)} ${chalk.gray(`[--mine]`)}
+  ${chalk.bold(`add`)} <dat-link|dat-ref>
+  ${chalk.bold(`rm`)} <dat-link|dat-ref>
+  ${chalk.bold(`host`)} <dat-link|dat-ref>
+  ${chalk.bold(`unhost`)} <dat-link|dat-ref>
+
+  ${chalk.blue(`Learn more at https://github.com/beakerbrowser/bkr`)}
+`)
+}
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,14 +8,24 @@ import chmod from 'gulp-chmod'
 
 gulp.task('help', help)
 
-gulp.task('compile', () =>
+gulp.task('compile', [
+  'compile-lib',
+  'compile-bin'
+])
+gulp.task('compile-lib', () =>
+  gulp.src('lib/**/*.js')
+  .pipe(babel())
+  .pipe(gulp.dest('build/lib')))
+gulp.task('compile-bin', () =>
   gulp.src('bin/*')
   .pipe(babel())
   .pipe(cropExt())
   .pipe(chmod(0o755))
   .pipe(gulp.dest('build/bin')))
 
-gulp.task('watch', () => gulp.watch(['bin/*', 'lib/**/*.js'], ['compile']))
+gulp.task('watch-lib', () => gulp.watch('lib/**/*.js', ['compile-lib']))
+gulp.task('watch-bin', () => gulp.watch('bin/*', ['compile-bin']))
+gulp.task('watch', ['watch-lib', 'watch-bin'])
 
 gulp.task('clean', () => del(['build']))
 
