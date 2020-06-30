af089cc8b7217ed89c34018f802c5c9be854cea0
Paul Frazee <pfrazee@gmail.com>
2020-06-30 12:34:13 -0500

    Add module loading from file or hyper

--- bin/bkr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-bin_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-lib_path=$( cd $bin_path ; cd ../lib; pwd -P )
-node --no-warnings --experimental-loader "${lib_path}/hyper-loader.js" ${bin_path}/bkr.js "$@"
