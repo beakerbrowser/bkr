d652e32738b1d495f12f656a37d527639e0ab3e8
Paul Frazee <pfrazee@gmail.com>
2020-06-30 00:42:49 -0500

    Add more of the beaker.hyperdrive API

--- test-scripts/hyperdrive.js
+++ test-scripts/hyperdrive.js
@@ -1,6 +1,21 @@
 main()
 async function main () {
-  console.log('Fetching my profile json...')
-  console.log(await beaker.hyperdrive.readFile('hyper://6900790c2dba488ca132a0ca6d7259180e993b285ede6b29b464b62453cd5c39/index.json', 'utf8'))
+  try {
+    console.log('Fetching...')
+    console.log(await beaker.hyperdrive.stat('hyper://a8e9bd0f4df60ed5246a1b1f53d51a1feaeb1315266f769ac218436f12fda830/index.json'))
+    console.log(await beaker.hyperdrive.readFile('hyper://a8e9bd0f4df60ed5246a1b1f53d51a1feaeb1315266f769ac218436f12fda830/index.json', 'utf8'))
+    console.log(await beaker.hyperdrive.readdir('hyper://a8e9bd0f4df60ed5246a1b1f53d51a1feaeb1315266f769ac218436f12fda830/'))
+    console.log(await beaker.hyperdrive.readdir('hyper://a8e9bd0f4df60ed5246a1b1f53d51a1feaeb1315266f769ac218436f12fda830/', {recursive: true}))
+    console.log(await beaker.hyperdrive.readdir('hyper://a8e9bd0f4df60ed5246a1b1f53d51a1feaeb1315266f769ac218436f12fda830/', {includeStats: true}))
+    var drive = beaker.hyperdrive.drive('hyper://a8e9bd0f4df60ed5246a1b1f53d51a1feaeb1315266f769ac218436f12fda830/')
+    console.log(await drive.stat('/index.json'))
+    console.log(await drive.readFile('/index.json', 'utf8'))
+    console.log(await drive.readdir('/'))
+    console.log(await drive.readdir('/', {recursive: true}))
+    console.log(await drive.readdir('/', {includeStats: true}))
+    console.log('done')
+  } catch (e) {
+    console.log('Error:', e)
+  }
   close()
 }
